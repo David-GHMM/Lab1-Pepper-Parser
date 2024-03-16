9c784af6b1530cfd9d4025dcc9bb9bce2137cd02
David-GHMM <138992075+David-GHMM@users.noreply.github.com>
2024-03-16 17:52:27 +0600

    Update pepper_parser.py

--- pepper_parser.py
+++ pepper_parser.py
@@ -8,7 +8,7 @@ data = {
     }
 
 headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
 
 
